f3a11a43e671857a1dbf94af4aff07bbf8f14cdf
Mahmoud Ahmed <mahmoudahmedxyz@gmail.com>
2023-01-31 17:33:39 +0200

    second commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    string str;
+    cout <<"Hello C++";
+    cin >> a >> b >> str;
+    cout <<a, b, str;
+    return 0;
+}
+
